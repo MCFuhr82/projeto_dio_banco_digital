75d8b5d4de88f127a93df7b06d835ce6ebb00301
Marcelo Cristo Fuhr <marcelinhofuhr@gmail.com>
2022-01-29 22:15:03 -0300

    criado lista contas

--- src/projeto_dio_banco_digital/Main.java
+++ src/projeto_dio_banco_digital/Main.java
@@ -1,31 +1,53 @@
 package projeto_dio_banco_digital;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
 public class Main {
 
 	public static void main(String[] args) {
 
 		Clientes marcelo = new Clientes("Marcelo");
-		
-		Contas cc = new ContaCorrente(marcelo);
-		Contas cp = new ContaPoupanca(marcelo);
-		
-		cc.depositar(100.00);
-		cc.imprimirExtrato();
+		Clientes rodrigo = new Clientes("Rodrigo");
+
+		Contas ccMarcelo = new ContaCorrente(marcelo);
+		Contas cpMarcelo = new ContaPoupanca(marcelo);
+
+		Contas ccRodrigo = new ContaCorrente(rodrigo);
+		Contas cpRodrigo = new ContaPoupanca(rodrigo);
+
+		ccMarcelo.depositar(100.00);
+		ccMarcelo.imprimirExtrato();
 		System.out.println();
-		
-		cp.depositar(50.00);
-		cp.imprimirExtrato();
+
+		cpMarcelo.depositar(50.00);
+		cpMarcelo.imprimirExtrato();
 		System.out.println();
-		
-		cc.transferir(60.00, cp);
-		
-		cp.imprimirExtrato();
+
+		ccMarcelo.transferir(60.00, cpRodrigo);
+
+		cpRodrigo.imprimirExtrato();
 		System.out.println();
-		cc.imprimirExtrato();
+		ccMarcelo.imprimirExtrato();
 		System.out.println();
-		
-		cp.sacar(100.00);
-		cp.imprimirExtrato();
+
+		ccRodrigo.depositar(230.00);
+		cpRodrigo.sacar(20.00);
+		cpRodrigo.imprimirExtrato();
+		System.out.println();
+
+		List<Contas> contas = new ArrayList<>();
+		contas.add(ccMarcelo);
+		contas.add(cpMarcelo);
+		contas.add(ccRodrigo);
+		contas.add(cpRodrigo);
+
+		for (Contas conta: contas) {
+			System.out.println(conta.cliente.getNome()
+					+ ": conta numero " + conta.numeroConta
+					+ " com saldo de R$ " + (String.format("%.2f", conta.getSaldo())));
+		}
 	}
 
 }
